3924722739b345fd1bb6f21b890e23b077ddc718
hades12580 <1534736887@qq.com>
2021-12-06 18:05:55 +0800

    add Jwt change

---
+++ src/main/java/com/markerhub/shiro/AccountRealm.java
@@ -0,0 +1,34 @@
+package com.markerhub.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountRealm extends AuthorizingRealm {
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof JwtToken;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        JwtToken jwtToken = (JwtToken) token;
+
+        System.out.println("-----------------------");
+
+        return null;
+    }
+}
