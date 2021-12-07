804b745f09ceee5d88cdf895df66769639aae90e
hades12580 <1534736887@qq.com>
2021-12-07 18:52:02 +0800

    add deal exception

--- src/main/java/com/markerhub/controller/UserController.java
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -4,6 +4,7 @@ package com.markerhub.controller;
 import com.markerhub.common.lang.Result;
 import com.markerhub.entity.User;
 import com.markerhub.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @RequiresAuthentication  //认证后访问
     @GetMapping("/index")
     public Result index() {
         User user = userService.getById(1L);
--- src/main/java/com/markerhub/shiro/AccountRealm.java
+++ src/main/java/com/markerhub/shiro/AccountRealm.java
@@ -1,16 +1,27 @@
 package com.markerhub.shiro;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+import com.markerhub.entity.User;
+import com.markerhub.service.UserService;
+import com.markerhub.util.JwtUtils;
+import lombok.SneakyThrows;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccountRealm extends AuthorizingRealm {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserService userService;
 
     @Override
     public boolean supports(AuthenticationToken token) {
@@ -22,13 +33,28 @@ public class AccountRealm extends AuthorizingRealm {
         return null;
     }
 
+    @SneakyThrows
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
 
         JwtToken jwtToken = (JwtToken) token;
 
+        String userId = jwtUtils.getClaimByToken((String) jwtToken.getPrincipal()).getSubject();
+
+        User user = userService.getById(Long.valueOf(userId));
+        if (user == null) {
+            throw new UnknownAccountException("账户不存在");
+        }
+
+        if (user.getStatus() == -1) {
+            throw new LockedAccountException("账户已被锁定");
+        }
+
+        AccountProfile profile = new AccountProfile();
+        BeanUtils.copyProperties(user, profile);
+
         System.out.println("-----------------------");
 
-        return null;
+        return new SimpleAuthenticationInfo(profile, jwtToken.getCredentials(), getName());
     }
 }
