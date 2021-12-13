3f200c570115d45f5bdb04b8030dcc88ddef869a
hades12580 <1534736887@qq.com>
2021-12-13 18:34:09 +0800

    add "实体校验处理+跨域处理+登录接口开发"

--- src/main/java/com/markerhub/controller/UserController.java
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -6,10 +6,8 @@ import com.markerhub.entity.User;
 import com.markerhub.service.UserService;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -30,6 +28,11 @@ public class UserController {
     @GetMapping("/index")
     public Result index() {
         User user = userService.getById(1L);
+        return Result.succ(user);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user) {
         return Result.succ(user);
     }
 }
