1974826783585b1135195a03a7f4307e73d1b5db
hades12580 <1534736887@qq.com>
2021-12-14 20:47:43 +0800

    add "后端接口"

--- src/main/java/com/markerhub/controller/BlogController.java
+++ src/main/java/com/markerhub/controller/BlogController.java
@@ -1,9 +1,17 @@
 package com.markerhub.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.markerhub.common.lang.Result;
+import com.markerhub.entity.Blog;
+import com.markerhub.service.BlogService;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,7 +22,47 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-12-03
  */
 @RestController
-@RequestMapping("/blog")
 public class BlogController {
+
+    @Autowired
+    BlogService blogService;
+
+    @GetMapping("/blogs")
+    public Result list(@RequestParam(defaultValue = "1") Integer currentPage) {
+
+        Page page = new Page(currentPage, 5);
+        IPage pageData = blogService.page(page, new QueryWrapper<Blog>().orderByDesc("created"));
+
+        return Result.succ(pageData);
+
+    }
+
+    @GetMapping("/blog/{id}")
+    public Result detail(@PathVariable(name = "id") Long id) {
+
+        Blog blog = blogService.getById(id);
+        Assert.notNull(blog, "该blog已被删除");
+
+        return Result.succ(blog);
+
+    }
+
+    @PostMapping("/blog/edit")
+    public Result edit(@Validated @RequestBody Blog blog) {
+
+        Blog temp = null;
+        if (blog.getId() != null) {
+            temp = blogService.getById(blog.getId());
+            // 只能编辑自己的文章
+            Assert.isTrue(temp.getUserId() == SecurityUtils.getSubject().getPrincipal());
+        }
+//        else {
+//            return null;
+//
+//        }
+
+        return Result.succ(null);
+
+    }
 
 }
